c81d3ebd04abf120810eaf9aa5b9cfb7297a0d06
neo <maiload77@gmail.com>
2024-09-11 15:51:32 +0900

    Part 2&3. 논리의 흐름 & 객체 지향 패러다임

---
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -0,0 +1,33 @@
+package cleancode.minesweeper.tobe;
+
+import cleancode.minesweeper.tobe.gamelevel.Beginner;
+import cleancode.minesweeper.tobe.gamelevel.GameLevel;
+import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
+import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
+import cleancode.minesweeper.tobe.io.InputHandler;
+import cleancode.minesweeper.tobe.io.OutputHandler;
+
+public class GameApplication {
+
+    public static void main(String[] args) {
+        GameLevel gameLevel = new Beginner();
+        InputHandler inputHandler = new ConsoleInputHandler();
+        OutputHandler outputHandler = new ConsoleOutputHandler();
+
+        Minesweeper minesweeper = new Minesweeper(gameLevel, inputHandler, outputHandler);
+        minesweeper.initialize();
+        minesweeper.run();
+    }
+
+    /**
+     * DIP (Dependency Inversion Principle)
+     * 고수준 모듈이 저수준 모듈을 직접 참조하는 것이 아닌 저수준 모듈을 추상화한 인터페이스를 참조
+     *
+     * DI (Dependency Injection)
+     * - 제 3자가 두 객체 간 의존성을 맺어주고 주입
+     *
+     * IoC (Inversion of Control)
+     * - 제어의 순방향 : 개개발자가 직접 프로그램의 흐름을 설계하고, 그 흐름을 주도
+     * - 제어의 역전 : 개발자가 작성한 코드는 프레임워크 안에 포함되어 하나의 톱니바퀴 역할을 하고, 그 흐름은 프레임워크가 주도
+     */
+}
